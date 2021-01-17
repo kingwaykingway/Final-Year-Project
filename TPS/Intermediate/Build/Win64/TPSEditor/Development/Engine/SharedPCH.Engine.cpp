0670d07fcde1784049c63cffe58b8844a98cfa4d
KingwayKingway <1040123009@qq.com>
2021-01-17 18:26:45 +0000

    2021/1/17 #1
    Implemented real-time target display on HUD in shooting range.
    ###Running the game will cause force quit. Next update will focus on fixing the issue.

--- TPS/Intermediate/Build/Win64/TPSEditor/Development/Engine/SharedPCH.Engine.cpp
+++ TPS/Intermediate/Build/Win64/TPSEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/kingw/OneDrive/Documents/Unreal Projects/TPS/Intermediate/Build/Win64/TPSEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Unreal Projects/Final-Year-Project/TPS/Intermediate/Build/Win64/TPSEditor/Development/Engine/SharedPCH.Engine.h"
